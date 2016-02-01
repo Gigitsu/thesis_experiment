083720fa123c06d42bb02517eaf457526f711152
Gigitsu <gigitsu.23@gmail.com>
2016-02-01 19:46:11 +0100

    Made max epochs configurable from starter creator script

--- create_experiment_starter.lua
+++ create_experiment_starter.lua
@@ -12,6 +12,7 @@ cmd:option('-max_layers', 5, 'The max number of layers to use')
 cmd:option('-min_seq_lengths', 60, 'The min value of sequence length to use')
 cmd:option('-max_seq_lengths', 100, 'The max value of sequence length to use')
 cmd:option('-seq_length_step', 20, 'The step value to use for increment sequence length')
+cmd:option('-max_epochs', 50, 'The max number of full passes through the training data')
 
 
 opt = cmd:parse(arg)
@@ -43,13 +44,19 @@ for _, d in pairs(datasets) do
       for s = opt.min_seq_lengths, opt.max_seq_lengths, opt.seq_length_step do
         commands = commands .. 'th train.lua'..
           ' -dataset '..d..
+          ' -max_epochs '..opt.max_epochs..
           ' -layers_number '..get_spaces(l, opt.max_layers)..l..
           ' -layer_size '..get_spaces(n, opt.max_nodes)..n..
           ' -seq_length '..get_spaces(s, opt.max_seq_lengths)..s..
           ' -checkpoint_dir cp_'..n..'h_'..l..'l_'..s..'s\n'
         if(opt.use_space) then
-          commands = commands ..
-            'th train.lua -use_space -seq_length '..s..' -layer_size '..n..' -layers_number '..l..' -checkpoint_dir cp_'..n..'h_'..l..'l_'..s..'s_ws -dataset '..d..'\n'
+            commands = commands .. 'th train.lua'..
+              ' -dataset '..d..
+              ' -max_epochs '..opt.max_epochs..
+              ' -layers_number '..get_spaces(l, opt.max_layers)..l..
+              ' -layer_size '..get_spaces(n, opt.max_nodes)..n..
+              ' -seq_length '..get_spaces(s, opt.max_seq_lengths)..s..
+              ' -use_space -checkpoint_dir cp_'..n..'h_'..l..'l_'..s..'s_ws\n'
         end
       end
     end
