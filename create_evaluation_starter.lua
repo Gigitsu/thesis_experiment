1b7d01a9ee84d0de1774d8f651d7e2a4d92ac179
Gigitsu <gigitsu.23@gmail.com>
2016-02-06 12:13:12 +0100

    Added lua script to generate evaluations

---
+++ create_evaluation_starter.lua
@@ -0,0 +1,54 @@
+require('./init.lua')
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Creates a shell script to start the evaluations')
+cmd:text()
+cmd:text('Options')
+-- data
+cmd:option('-output_number', 1, 'output number per char to use for extract classes')
+cmd:text()
+
+opt = cmd:parse(arg)
+
+local models = {}
+local evaluations = {}
+
+for ds in g2.dirs(g2.SAVE_DIR) do
+  for cp in g2.dirs(ds) do
+    local min_loss, min_path = 100, ''
+    for t7 in g2.files(cp, '.t7') do
+      local loss = tonumber(t7:sub(#t7-8, #t7-3))
+      if(loss and loss < min_loss) then
+        min_loss = loss
+        min_path = path.join(cp, t7)
+      end
+    end
+    table.insert(models, min_path)
+  end
+end
+
+local commands = ''
+
+for _, m in pairs(models) do
+  local b = m:sub(1, m:len() -  3)
+  local e = b..'_evaluation.t7'
+  local c = b..'_classification_using_'..opt.output_number..'_outputs.t7'
+  if(not paths.filep(e)) then
+    commands = commands..
+      'th evaluate.lua '..m..'\n'
+  end
+  if(not paths.filep(c)) then
+    commands = commands..
+      'th classify.lua '..e..' -output_number '..opt.output_number..'\n'
+  end
+end
+
+local filename = './evaluation_starter.sh'
+local file = require("pl.file")
+file.write(filename, commands)
+
+require('fs')
+fs.chmod(filename, 755)
+
+print('Evaluation starter script created successfully')
